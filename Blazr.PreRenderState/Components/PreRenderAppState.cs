7efe3ea824fd0b6ab81e40b4ed283a71f70c1eff
Shaun Curtis <shaun@coldelm.com>
2024-03-18 07:39:24 +0000

    Add project files.

---
+++ Blazr.PreRenderState/Components/PreRenderAppState.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazr.PreRenderState.Components;
+
+public class PreRenderAppState
+{
+    public const string TokenName = "IPAddress";
+
+    public string RemoteIPAddress { get; set; } = "Not Set";
+
+    public void Load(PreRenderAppState state)
+    {
+        this.RemoteIPAddress = state.RemoteIPAddress;
+    }
+}
---
+++ Blazr.PreRenderState/Components/PreRenderStateLoader.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazr.PreRenderState.Components;
+
+public class PreRenderStateLoader : ComponentBase
+{
+    [Inject] public PreRenderAppState AppState { get; set; } = default!;
+    [Inject] public PersistentComponentState ApplicationState { get; set; } = default!;
+
+    private bool _subsequentRender;
+
+    // Short circuit all the lifecycle stuff - we don't need it
+    public override Task SetParametersAsync(ParameterView parameters)
+    {
+        if (_subsequentRender)
+            return Task.CompletedTask;
+
+        // if not prerender, try and get the persisted value
+        if (this.ApplicationState.TryTakeFromJson<PreRenderAppState>(PreRenderAppState.TokenName, out var state))
+        {
+            if (state is not null)
+                this.AppState.Load(state);
+        }
+
+        _subsequentRender = true;
+        return Task.CompletedTask;
+    }
+}
